8320dc1ed44c97c86d0c2a2d7c381c5ddee5d9ac
SReateguiUtec <sebastian.reategui@utec.edu.pe>
2026-08-05 17:26:02 -0500

    linkedin added

--- src/components/ui/hero-ascii.tsx
+++ src/components/ui/hero-ascii.tsx
@@ -354,7 +354,14 @@ export default function HeroAscii() {
                                     GitHub
                                 </a>
 
-                                <a href="mailto:reateguisebastian1@gmail.com" className="relative flex min-h-11 items-center justify-center gap-2 px-3 py-2 lg:px-6 lg:py-2.5 bg-transparent border border-white text-white font-mono text-xs lg:text-sm hover:bg-white hover:text-black transition-all duration-200">
+                                <a href="https://www.linkedin.com/in/sebasreateguib" target="_blank" rel="noopener noreferrer" className="relative flex min-h-11 items-center justify-center gap-2 px-3 py-2 lg:px-6 lg:py-2.5 bg-transparent border border-white text-white font-mono text-xs lg:text-sm hover:bg-white hover:text-black transition-all duration-200">
+                                    <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                                    </svg>
+                                    LinkedIn
+                                </a>
+
+                                <a href="mailto:reateguisebastian1@gmail.com" className="relative col-span-2 flex min-h-11 items-center justify-center gap-2 px-3 py-2 lg:px-6 lg:py-2.5 bg-transparent border border-white text-white font-mono text-xs lg:text-sm hover:bg-white hover:text-black transition-all duration-200">
                                     <Mail className="w-4 h-4" />
                                     Email
                                 </a>
